15530e664b3fb6dfd55db1938aa1aa65b4352890
KvisazGames <kiko@kiko.kiko>
2020-06-04 17:20:27 +0700

    kvisaz  - wonder 0.6.25 fix

--- src/data/actions/story-format.js
+++ src/data/actions/story-format.js
@@ -177,8 +177,8 @@ const actions = (module.exports = {
 		const builtinFormats = [
 			{
 				name: 'Wonder',
-				url: 'story-formats/wonder-0.6.24/format.js',
-				version: '0.6.24',
+				url: 'story-formats/wonder-0.6.25/format.js',
+				version: '0.6.25',
 				userAdded: false
 			},
 			{
@@ -258,7 +258,7 @@ const actions = (module.exports = {
 			setPref(
 				store,
 				'defaultFormat',
-				{name: 'Wonder', version: '0.6.24'}
+				{name: 'Wonder', version: '0.6.25'}
 			);
 		}
 
